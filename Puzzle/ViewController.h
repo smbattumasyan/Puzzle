5af9fb2ab626b9322427f19e23af7577f660c185
Smbat Tumasyan <smbatt@davitg-mac.local>
2016-03-10 15:53:25 +0400

    Initial Commit

---
+++ Puzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Puzzle
+//
+//  Created by Smbat Tumasyan on 3/10/16.
+//  Copyright © 2016 EGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
